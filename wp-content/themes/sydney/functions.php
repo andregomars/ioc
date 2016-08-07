f54aa48291f2891ded5b204583280835fe35b3b3
andre <andregomars@gmail.com>
2016-08-06 17:20:11 -0700

    insert custom cookie for ioc

--- wp-content/themes/sydney/functions.php
+++ wp-content/themes/sydney/functions.php
@@ -445,82 +445,6 @@ function remove_help($old_help, $screen_id, $screen){
 add_filter( 'contextual_help', 'remove_help', 999, 3 );
 
 
-
-
-/*
- * add external auth through API GET, please refer to
- * https://github.com/WordPress-Seattle/WordPress-Login/blob/master/external-wp-auth-client/external-wp-auth.php
- */
-$query_var = "ext_auth";
-$auth_url = "http://localhost:52432/api/IOCUser";
-
-// Setup the external auth
-//add_filter( 'authenticate', 'extwpauth_authenticate', 10, 3 );
-function extwpauth_authenticate( $user, $username, $password) {
-	error_log('call extwpauth_authenticate');
-
-    global $auth_url;
-    // Make sure a username and password are present for us to work with
-    if($username == '' || $password == '') return;
- 	
- 
-    $response = wp_remote_get( "$auth_url?loginname=$username&password=$password" );
-
-    $ext_auth = json_decode( $response['body'], true);
-
-//by andre
- //error_log('response: '.print_r($response, 1));
- //error_log('ext_auth: '.print_r($ext_auth, 1));
-//by andre end
-
-     if( !$response ) {
-        // User does not exist,  send back an error message
-        $user = new WP_Error( 'denied', __("<strong>Invalid External Login</strong>: User/pass bad") );
-     } else {
-         // External user exists, try to load the user info from the WordPress user table
-         // $userobj = new WP_User();
-         // $user = $userobj->get_data_by( 'login', 'fm_'.$ext_auth['data']['user_login'] ); // Does not return a WP_User object <img src='http://ben.lobaugh.net/blog/wp-includes/images/smilies/icon_sad.gif' alt=':(' class='wp-smiley' />
-         // $user = new WP_User($user->ID); // Attempt to load up the user with that ID
-         // if( $user->ID == 0 ) {
-         //     // The user does not currently exist in the WordPress user table.
-         //     // You have arrived at a fork in the road, choose your destiny wisely
- 
-         //     // If you do not want to add new users to WordPress if they do not
-         //     // already exist uncomment the following line and remove the user creation code
-         //     //$user = new WP_Error( 'denied', __("<strong>ERROR</strong>: Not a valid user for this system") );
- 
-         //     // Setup the minimum required user information for this example
-         //     $userdata = array( 'user_email' => $ext_auth['data']['email'],
-         //                        'user_login' => "fm_".$ext_auth['data']['user_login'],
-         //                        //'first_name' => $ext_auth['first_name'],
-         //                        //'last_name' => $ext_auth['last_name']
-         //                        'user_nicename' => "FM_" . $ext_auth['data']['user_nicename'],
-         //                        'role' => $ext_auth['roles'][0]
-         //                        );
-         //     //$new_user_id = wp_insert_user( $userdata ); // A new user has been created
- 
-         //     // Load the new user info
-         //     $user = new WP_User ($new_user_id);
-         // } 
-
-
-		$user = new WP_User();
-        $user->ID = $ext_auth['ID'];
-        $user->allcaps = $ext_auth['allcaps'];
-        $user->cap_key = $ext_auth['cap_key'];
-        $user->caps = $ext_auth['caps'];
-        $user->data = $ext_auth['data'];
-        $user->filter = $ext_auth['filter'];
-        $user->roles = $ext_auth['roles'];
-     }
- 
-     // Comment this line if you wish to fall back on WordPress authentication
-     // Useful for times when the external service is offline
-     //remove_action('authenticate', 'wp_authenticate_username_password', 20);
-
-     return $user;
-}
-
 //add_filter( 'authenticate', 'wpapi_auth', 10, 3 );
 function wpapi_auth( $user, $username, $password) {
 	global $wpapi;
@@ -584,7 +508,6 @@ function wpapi_auth( $user, $username, $password) {
 
 	return $user;
 }
-
 
 
 //by andre end
