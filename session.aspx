d74803fd9189319e010ccc0b253d9314ca5fe2ec
andre <andregomars@gmail.com>
2016-08-07 01:54:50 -0700

    update session.aspx to show userinfo from api

--- session.aspx
+++ session.aspx
@@ -4,11 +4,34 @@
 
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="container">
-        <h1 class="text-center">mockup page</h1>
-        <input type="text" id="txtName" />
-        <input type="text" id="txtPassword" />
-        <input type="submit" value="Submit" />
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Mockup</a>
+            </div>
+        </div>
+     </div>
+     <div class="container body-content">
+         <div class="jumbotron">
+            <h1>Session Mockup</h1>
+            <p class="lead">Test cookie passing between PHP and ASP.Net served pages</p>
+            <p class="lead">&nbsp;</p>
+            <p class="lead">&nbsp;</p>
+            <p>Current Logged In User: <mark><asp:Label runat="server" ID="lbl_UserName" /></mark> </p>
+            <p>
+                <a href="http://localhost/ioc" class="btn btn-primary btn-lg">I/O Control Homepage</a>
+                <a href="http://localhost/ioc/login.php?action=logout" class="btn btn-primary btn-lg">Logout</a>
+            </p>
+        </div>
+     </div>
+     <div class="row">
+         <div class="col-md-4">
+             <p>
+                 <asp:Label runat="server" ID="lbl_UserInfo" />
+             </p>
+         </div>
+         <div class="col-md-4">&nbsp</div>
+         <div class="col-md-4">&nbsp</div>
      </div>
 </asp:Content>
 
--- session.aspx.cs
+++ session.aspx.cs
@@ -4,21 +4,53 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.Text;
+using System.IO;
 
 public partial class _Default : System.Web.UI.Page
 {
     const string IOC_LOGGED_IN_COOKIE = "ioc_looggedin";
+    const string Base_API_URI = "http://localhost:52432/";
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        string username = GetCookie(IOC_LOGGED_IN_COOKIE); 
-        Response.Write("username is: " + username);
-
+        if(!IsPostBack)
+        {
+            string username = GetCookie(IOC_LOGGED_IN_COOKIE); 
+            lbl_UserName.Text = username;
+            
+            string userinfo = GetUserInfo(username);
+            lbl_UserName.ToolTip = userinfo;
+        }
     }
 
     public string GetCookie(string cookieName) 
     {
         var cookie = Request.Cookies[cookieName];
-        return cookie == null ? null :  cookie.Value;
+        return cookie == null ? "N/A" :  cookie.Value;
+    }
+
+    public string GetUserInfo(string username)
+    {
+        string url_userinfo = Base_API_URI + "api/IOCUserInfo?loginName=" + username;
+
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url_userinfo);
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader( stream );
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+        catch (WebException ex)
+        {
+            return ex.Message;
+        }
+
     }
 }
