e792bc6e81488e1afbd4162833ba6319f8ddc75b
andre <andregomars@gmail.com>
2016-08-06 23:11:26 -0700

    add aspx for sessing mockup

---
+++ session.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
+    CodeFile="session.aspx.cs" Inherits="_Default" 
+   MasterPageFile="~/MasterPage.master" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <h1 class="text-center">mockup page</h1>
+        <input type="text" id="txtName" />
+        <input type="text" id="txtPassword" />
+        <input type="submit" value="Submit" />
+     </div>
+</asp:Content>
+
---
+++ session.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    const string IOC_LOGGED_IN_COOKIE = "ioc_looggedin";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string username = GetCookie(IOC_LOGGED_IN_COOKIE); 
+        Response.Write("username is: " + username);
+
+    }
+
+    public string GetCookie(string cookieName) 
+    {
+        var cookie = Request.Cookies[cookieName];
+        return cookie == null ? null :  cookie.Value;
+    }
+}
